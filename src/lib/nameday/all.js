91f983744bf8a7b5ed19e8a29d6e26c2955cde97
rogerbf <roger@bergfalk.nu>
2016-09-21 15:39:03 +0200

    dynamic page count

--- src/lib/nameday/all.js
+++ src/lib/nameday/all.js
@@ -2,11 +2,30 @@ const sequence = require(`sequential-resolve`)
 const range = require(`prefixed-range`)
 const xray = require(`./xray.js`)
 
-const pageCount = 37
-
 const paginationUrl = `http://www.svenskaakademien.se/svenska-akademien/almanackan/akademialmanackan/namnlista?page=`
 
-const scrapeNames = url => new Promise((resolve, reject) => {
+const nameCount = () => new Promise((resolve, reject) => {
+  xray(`${paginationUrl}0`, `.view-name-day-list .view-header`)((err, str) => {
+    if (err) reject(err)
+    resolve({ nameCount: parseInt(str.replace(/\D/g, ``)) })
+  })
+})
+
+const namesPerPage = () => new Promise((resolve, reject) => {
+  xray(`${paginationUrl}0`, [`tbody tr`])((err, rows) => {
+    if (err) reject(err)
+    resolve({ perPage: rows.length })
+  })
+})
+
+const pageCount = () => new Promise((resolve, reject) => {
+  Promise.all([nameCount(), namesPerPage()])
+    .then(arr => Promise.resolve(Object.assign({}, arr[0], arr[1])))
+    .then(stats => resolve(Math.floor(stats.nameCount / stats.perPage)))
+    .catch(e => reject(e))
+})
+
+const names = url => new Promise((resolve, reject) => {
   xray(url, `tbody tr`, [{
     name: `a`,
     date: `.views-field-field-date-ymd | trim | parseDate`,
@@ -18,7 +37,7 @@ const scrapeNames = url => new Promise((resolve, reject) => {
   })
 })
 
-module.exports = () => sequence(
-  scrapeNames,
-  range(paginationUrl, { start: 0, end: pageCount })
-)
+module.exports = () => pageCount()
+  .then(count => {
+    return sequence(names, range(paginationUrl, { start: 0, end: count }))
+  })
